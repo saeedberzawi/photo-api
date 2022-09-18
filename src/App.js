c5bef8b96094c089d3b082a90c5c4127ad65bb74
Saeed Al Berzawi <saidberzawi@gmail.com>
2022-09-18 08:58:44 +0300

    18/9

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
-import "./App.css";
+import React, { useState, useEffect } from "react";
+// import { FaSearch } from "react-icons";
+
+const clintID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`;
+const mainUrl = `https://api.unsplash.com/photos/`;
+const searchUrl = `https://api.unsplash.com/search/photos/`;
 
 function App() {
-  return <div className="App"></div>;
+  const [loading, setLoading] = useState(false);
+  const [photo, setPhoto] = useState([]);
+  const fetchImages = async () => {
+    let url;
+    url = `${mainUrl}${clintID}`;
+    try {
+      setLoading(true);
+      const response = await fetch(url);
+      const data = response.json();
+      console.log(data);
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    fetchImages();
+  }, []);
+  return <div className="App"><p>hello world</p></div>;
 }
 
 export default App;
